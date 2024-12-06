8e5c94498ef6258ae6de248d2ddc73d4435a5993
daniil <polyntsevdaniil@gmail.com>
2024-12-06 17:33:32 +0700

    vowel check

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -28,14 +28,7 @@ class MainActivity : AppCompatActivity() {
 
             val char = input[0]
 
-            if (char.isUpperCase()) {
-                when (char) {
-                    'L', 'M', 'K', 'D' -> resultText.text = "Это согласные буквы"
-                    else -> resultText.text = "Возможно, это гласные буквы"
-                }
-            } else {
-                Toast.makeText(this, "Введите латинскую заглавную букву", Toast.LENGTH_SHORT).show()
-            }
+
         }
     }
 }
